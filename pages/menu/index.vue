592dd2b9ec610abf973a14567bca93cbad82db72
chentong <chentong19950106@gmail.com>
2022-11-18 16:02:38 +0800

    test

---
+++ pages/menu/index.vue
@@ -0,0 +1,27 @@
+<template>
+	<view>
+		<view @click="toHome()">
+			菜单1
+		</view>
+		<view @click="toHome()">
+			菜单2
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
